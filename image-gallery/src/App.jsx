b614bde06a615e10681e2e6fec3199ddcbde6607
samulinukala <samulinukala740@gmail.com>
2025-12-15 15:54:04 +0200

    worked on random image gallery. made component that randomly returns an image

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -2,33 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+function RandomImage(num)
+{
+  console.log(num);
+  if(num>=0)
+  { 
+  return <img src={`https://picsum.photos/200/200?random${num}`}></img>
+  }
+  else
+  {
+    return <img src={"https://picsum.photos/200/200"}></img>
+  }
+  
+}
+function RandomImageList(count)
+{
+  let images=[];
+  for (let i=0; i<count;i++){
+    images.push(i);
+  }
+  return 
+  (
+    <div>
+    {images.map(numv=><RandomImage num={numv} />)}
+    </div>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
-    <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>gallery</h1>
+     <RandomImage />
+    <RandomImageList count={100}/>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+   
   )
 }
 

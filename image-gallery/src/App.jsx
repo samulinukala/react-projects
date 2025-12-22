c3a3b78e8bf440f312afd3a6e224fd3f1d47903c
samulinukala <samulinukala740@gmail.com>
2025-12-22 13:07:16 +0200

    got the base component working

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-function RandomImage(num)
+function RandomImage(props)
 {
-  console.log(num);
-  if(num>=0)
+  const style={marginTop: "0.2%",marginLeft:"0.9%"}
+  if(props.num??0)
   { 
-  return <img src={`https://picsum.photos/200/200?random${num}`}></img>
+    console.log(props.num);
+  return <img style={style} src={`https://picsum.photos/200/200?random${props.num}`}></img>
   }
   else
   {
@@ -15,19 +16,51 @@ function RandomImage(num)
   }
   
 }
-function RandomImageList(count)
+function SpecificImage(props)
+{
+  const style={marginTop: "0.2%",marginLeft:"0.9%", width:"300px", height:"378px"}
+  
+   return <img style={style} src={props.url}></img>
+}
+function DriveImage(props)
+{
+  const style={marginTop: "0.2%",marginLeft:"0.9%"}
+  const PicStyle={width:"300px", height:"378px"}
+  const shitUrl=props.url;
+  console.log(shitUrl);
+  const cleanerUrl=shitUrl.slice(32,65);
+  console.log(cleanerUrl);
+   return (
+     <div style={style}>
+     <img style={PicStyle} src={`https://lh3.googleusercontent.com/u/0/d/${cleanerUrl}`}></img>
+     {props.title&& <p>{props.title} </p>}
+     </div>
+     )
+}
+function RandomImageList(props)
 {
   let images=[];
-  for (let i=0; i<count;i++){
+  for (let i=0; i<props.count;i++){
+    console.log("looping at "+i)
     images.push(i);
+  
   }
-  return 
-  (
+  console.log(images.length);
+  return (
     <div>
-    {images.map(numv=><RandomImage num={numv} />)}
+    {images.map((numv)=>(
+      <RandomImage num={numv} />
+      
+      ))}
     </div>
   )
 }
+function DriveImageRenderList(props)
+{
+  
+}
+
+  <RandomImageList count={150}/>
 function App() {
 
 
@@ -36,8 +69,8 @@ function App() {
 
       <div>
       <h1>gallery</h1>
-     <RandomImage />
-    <RandomImageList count={100}/>
+    
+    <DriveImage url="https://drive.google.com/file/d/1fSnJgRD0h1XuJvjEXUfBa49lld31qkud/view?usp=sharing" title="zia simpson"/>
 
       </div>
     
